3be98e62d7fb27fc4d8a35b3f71383580335c992
Deden Rustiana <rustianadeden25@gmail.com>
2022-06-16 04:42:08 +0700

    Initial Project

---
+++ src/main/java/com/springbootsoap/sbsoap/SbSoapApplication.java
@@ -0,0 +1,13 @@
+package com.springbootsoap.sbsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootsoap/sbsoap/SbSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootsoap.sbsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
